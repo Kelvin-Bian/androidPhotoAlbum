adb8ab726fe44e66bc2c554c87ecd0e83ddab691
jluo3364 <77076660+jluo3364@users.noreply.github.com>
2023-12-06 01:08:05 -0500

    .idea files

--- app/src/main/java/com/example/testphotos/PhotoViewerFragment.java
+++ app/src/main/java/com/example/testphotos/PhotoViewerFragment.java
@@ -7,28 +7,33 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.testphotos.classes.Photo;
 import com.example.testphotos.classes.Tag;
-import com.example.testphotos.databinding.FragmentFirstBinding;
+import com.example.testphotos.databinding.PhotoviewerBinding;
 
 import java.util.ArrayList;
 
 public class PhotoViewer extends Fragment {
-    private FragmentFirstBinding binding;
+    private PhotoviewerBinding binding;
 
     private RecyclerView locationTagValues;
     private RecyclerView personTagValues;
     private Photo p;
     private ArrayList<Tag> locationTags;
     private ArrayList<Tag> personTags;
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
-        locationTagValues = container.findViewById(R.id.locationTagValues);
+        binding = PhotoviewerBinding.inflate(inflater, container, false);
+        locationTagValues = binding.locationTagValues;
+        locationTagValues.setLayoutManager(new LinearLayoutManager(requireContext()));
         locationTagValues.setAdapter(new TagAdapter());
 
-        personTagValues = container.findViewById(R.id.personTagValues);
+        personTagValues =binding.personTagValues;
+        personTagValues.setLayoutManager(new LinearLayoutManager(requireContext()));
         personTagValues.setAdapter(new TagAdapter());
 
         locationTags = new ArrayList<>();
---
+++ app/src/main/java/com/example/testphotos/PhotoViewerViewModel.java
@@ -0,0 +1,4 @@
+package com.example.testphotos;
+
+public class PhotoViewerController {
+}
